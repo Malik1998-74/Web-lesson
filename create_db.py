5ccd630aa9eadb12812ff83fcace6ccfca369f95
Malik Nizamov <malik.nizamovv@gmail.com>
2023-06-20 20:47:02 +0300

    переделал базу данных ЗАРАБОТАЛ Flask-SQLAlchemy

--- create_db.py
+++ create_db.py
@@ -1,3 +1,12 @@
-from webapp import db, create_app
+from flask import Flask
+# from webapp import db, create_app
+from webapp.__init__ import create_app
+from webapp.model import db
 
-db.create_all(app=create_app())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app) 
+
+    with app.app_context():
+        db.create_all()
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-from webapp.model import db
-from webapp.python_org_news import get_python_news
+from webapp.model import db, News
+# from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
 def create_app():
@@ -9,12 +9,15 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)    # инициализируем нашу базу данных 
 
+    # with app.app_context():
+    #     db.create_all()
+
 
     @app.route('/')
     def index():
         title = 'Новости Python'   # короче это у нас главная страница
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])  # переменнаы погода получилось с помощью функции
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template("index.html", page_title=title, weather=weather, news_list=news_list)  # выполняет рендеринг указанного шаблона и добавляет результат в ответ возвращает шаблон то что видим html название и функцию погоды
 
     return app
